b3f7301dd05feb3a32fd90bb62501aac2ec3a27d
Italo Gama <italoflorencio@gmail.com>
2024-09-03 15:54:04 -0300

    changing payment form to handle pix

--- components/payment-form.tsx
+++ components/payment-form.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -11,27 +10,25 @@ import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Icons } from "./ui/icons";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
-import { Calendar } from "./ui/calendar";
-import { CalendarIcon } from "lucide-react";
-import { format } from "date-fns";
-import ptBR from "date-fns/locale/pt-BR";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
-import { Checkbox } from "./ui/checkbox";
-import { Card } from "./ui/card";
 import Swal from "sweetalert2";
-
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
-import RegisterHeader from "./register-header";
-import { civilStatusType, phoneRegex, tshirtSizes } from "@/lib/helpers/helpers";
-import { TextDivider } from "./text-divider";
-import PaymentHeader from "./payment-header";
 import Link from "next/link";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import PaymentHeader from "./payment-header";
 
 interface PaymentFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const paymentFormSchema = z.object({
-  email: z.string({ required_error: "Você deve informar seu email" }).email({ message: "Você deve preencher um email válido" }),
+  email: z
+    .string({ required_error: "Você deve informar seu email" })
+    .email({ message: "Você deve preencher um email válido" }),
 });
 
 type RegisterFormValues = z.infer<typeof paymentFormSchema>;
@@ -79,21 +76,45 @@ export function PaymentForm({ className, ...props }: PaymentFormProps) {
           confirmButtonText: "Ok",
         });
       } else {
-        const mercadopago = await fetch("/api/mercadopago", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email: values.email }),
+        const result = await Swal.fire({
+          title: "Pagamento",
+          text: "Deseja continuar para a página de pagamento?",
+          icon: "warning",
+          html: `
+          <div style="display: flex; align-items: center;">
+            <img height="150px" width="150px" src="https://firebasestorage.googleapis.com/v0/b/icr-members-c94f2.appspot.com/o/qrcode-pix.png?alt=media&token=d0520ae3-ebd3-4f31-a84c-23ae16047172" alt="QRCodeICR" style="margin-right: 20px;">
+            <p>Utilize este QRCode para realizar o pagamento via <b>PIX</b>. Se o método de pagamento for outro, por favor prossiga para a próxima tela.</p>
+          </div>
+          <p><b>Pix Copia-e-cola:</b></p>
+          <p>
+          00020126430014BR.GOV.BCB.PIX0121cursilhoicr@gmail.com5204000053039865406350.005802BR5901N6001C62150511CursilhoICR63044AC8</p>
+          <p style="color:red;"><b>Atenção</b>:</p>
+          <p>Enviar o comprovante do PIX para: (81) 99971-6016</p>
+        `,
+          showCancelButton: true,
+          confirmButtonText: "Prosseguir para pagamento ",
+          cancelButtonText: "Fechar",
         });
 
-        const url = await mercadopago.json();
-        window.location.replace(url.data);
+        if (result.isConfirmed) {
+          const mercadopago = await fetch("/api/mercadopago", {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email: values.email }),
+          });
+
+          const url = await mercadopago.json();
+          window.location.replace(url.data);
+        }
+
         setIsLoading(false);
       }
     } catch (error) {
       console.log(error);
+      setIsLoading(false);
     }
   }
 
@@ -111,9 +132,14 @@ export function PaymentForm({ className, ...props }: PaymentFormProps) {
                   <FormItem>
                     <FormLabel htmlFor="email">Email</FormLabel>
                     <FormControl>
-                      <Input id="email" placeholder="Digite seu email" type="email" disabled={isLoading} {...field} />
+                      <Input
+                        id="email"
+                        placeholder="Digite seu email"
+                        type="email"
+                        disabled={isLoading}
+                        {...field}
+                      />
                     </FormControl>
-
                     <FormMessage />
                   </FormItem>
                 )}
@@ -121,12 +147,21 @@ export function PaymentForm({ className, ...props }: PaymentFormProps) {
             </div>
           </div>
           <Button disabled={isLoading} type="submit" className="w-full mt-5">
-            {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
             Confirmar
           </Button>
           <Link href="/">
-            <Button disabled={isLoading} variant={"link"} type="submit" className="w-full mt-3">
-              {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
+            <Button
+              disabled={isLoading}
+              variant={"link"}
+              type="submit"
+              className="w-full mt-3"
+            >
+              {isLoading && (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              )}
               Voltar para página inicial
             </Button>
           </Link>
